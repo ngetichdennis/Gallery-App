c70dff56619cc4ece1b0490ba147dfe6deccbac8
Dennis <ngetichdennis90@gmail.com>
2024-02-14 13:55:51 +0300

    adding a function which handles submit of form data

--- galleryapp/src/components/AddForm.jsx
+++ galleryapp/src/components/AddForm.jsx
@@ -1,7 +1,17 @@
 import {useState} from "react";
 function AddForm(){
     //initial form data object with empty fields and current date as 'date_added' field
-    const [formData,setFormData]= useState({name:"",image:"",description:"",date_added:Date.now()});  
+    const [formData,setFormData]= useState({name:"",image:"",description:"",date_added:Date.now()}); 
+        //Handling form submission
+        function handleSubmit(event){
+            //preventing  default page refresh on submit
+            event.preventDefault()
+            //passing the form data to parent component for further processing
+            onsubmit({...formData})
+            //reseting the form
+            setFormData("")
+        }
+     
     return(
 
     )

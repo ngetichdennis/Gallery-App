2eec355098617b79df93bb993faebf3ee3ec88ed
Dennis <ngetichdennis90@gmail.com>
2024-02-14 13:57:40 +0300

    adding a function to handle form input changes

--- galleryapp/src/components/AddForm.jsx
+++ galleryapp/src/components/AddForm.jsx
@@ -2,6 +2,11 @@ import {useState} from "react";
 function AddForm(){
     //initial form data object with empty fields and current date as 'date_added' field
     const [formData,setFormData]= useState({name:"",image:"",description:"",date_added:Date.now()}); 
+    //handle input changes
+    function handleChange(e){
+        const {name,value}=e.target
+        setFormData({...formData, [name]: value}) 
+    }
         //Handling form submission
         function handleSubmit(event){
             //preventing  default page refresh on submit

0fc220b8d0b12320b7278268aa8f0638f67ab29c
Dennis <ngetichdennis90@gmail.com>
2024-02-14 13:49:41 +0300

    importing search Bar component,setting state for the images

--- galleryapp/src/components/App.jsx
+++ galleryapp/src/components/App.jsx
@@ -1,10 +1,21 @@
-import {} from "react"
+import {useState} from "react"
 import SearchBar from "./SearchBar";
+import AddForm from  './AddForm';
 function App(){
+    const [images,setImages]= useState([]);
+    //handle adding new images to the state
+    function handleAddImages(images){
+        //new images declaration
+        const imagesAdded={...images}
+        //adding new state to the existing state
+        setImages([...images,imagesAdded]);
+    }
+
     return(
         <div>
             <h1>Gallery App</h1>
             <SearchBar/>
+            <AddForm onSubmit ={handleAddImages} />
         </div>
 
     )
--- galleryapp/src/components/SearchBar.jsx
+++ galleryapp/src/components/SearchBar.jsx
@@ -1,5 +1,6 @@
 function SearchBar(){
     return(
+        <></>
 
     )
 }

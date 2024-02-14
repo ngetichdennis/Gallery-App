fb8bf2793cb6cc245175cb9b75a2f2ceb7baa1bf
Mariam Senzia <mariamsenzia@gmail.com>
2024-02-14 16:59:28 +0300

    modified app.jsx

--- galleryapp/src/components/App.jsx
+++ galleryapp/src/components/App.jsx
@@ -1,15 +1,38 @@
-import {} from "react"
+import {useEffect,useState} from "react"
 import SearchBar from "./SearchBar";
 import SignUp from "./SignUp";
+import GalleryCollection from "./GalleryCollection";
+import AddForm from "./AddForm";
+
 function App(){
+    ///gallery contains the images
+    const [gallery,setGallery] = useState([]);
+    const [images,setImages]= useState([]);
+    //handle adding new images to the state
+    function handleAddImages(images){
+        //new images declaration
+        const imagesAdded={...images}
+        //adding new state to the existing state
+        setImages([...images,imagesAdded]);
+    }
+    
+    useEffect(() => {
+        fetch(" http://localhost:3000/Gallery")
+        .then((res) => res.json())
+        .then((data) => {
+            console.log(data)
+            setGallery(data)
+        });
+        
+    },[])
+
     return(
         <div>
             <h1>Gallery App</h1>
-            <SearchBar/>
             <SignUp />
-            
-
-
+            <SearchBar/>
+             <GalleryCollection gallery={gallery}/>
+             <AddForm onAddImage ={handleAddImages} />
         </div>
 
     )

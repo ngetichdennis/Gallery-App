56b7c1a6cbb33145226f19d305fd4cfd54dde51d
denniskioko <kiokodennis6990@gmail.com>
2024-02-15 13:46:20 +0300

    adding theme toggle

--- galleryapp/src/components/ThemeToggle.jsx
+++ galleryapp/src/components/ThemeToggle.jsx
@@ -1,4 +1,31 @@
+import React from 'react';
+import { useColorMode, Switch, Button } from '@chakra-ui/react';
 
+const ThemeToggle = () => {
+  const { colorMode, toggleColorMode } = useColorMode();
+
+  // Toggle color mode when the button is clicked
+  const handleToggleMode = () => {
+    toggleColorMode(); 
+  };
+
+  return (
+    <div>
+    
+      <Switch
+        colorScheme="teal"
+        isChecked={colorMode === 'dark'}
+        onChange={toggleColorMode}
+      />
+      {/* Button to toggle light mode specifically */}
+      <Button onClick={handleToggleMode} ml={3}>
+        {colorMode === 'dark' ? 'Light' : 'Dark'} Mode
+      </Button>
+    </div>
+  );
+};
+
+export default ThemeToggle;
 
 
 
